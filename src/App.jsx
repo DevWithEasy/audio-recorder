fac37b00918e3c945cf2fae71327048423a21459
DevWithEasy <devwitheasy@gmail.com>
2023-12-20 00:37:16 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,110 @@
-import { useEffect, useRef, useState } from 'react';
-import { MdOutlineSettingsVoice, MdOutlinePause } from "react-icons/md";
-import { IoStopOutline } from "react-icons/io5";
-import { CiPlay1 } from "react-icons/ci";
-import { GrPowerReset } from "react-icons/gr";
+import { useEffect, useRef, useState } from 'react'
+import { MdOutlineSettingsVoice, MdOutlinePause } from "react-icons/md"
+import { IoStopOutline } from "react-icons/io5"
+import { CiPlay1 } from "react-icons/ci"
+import { GrPowerReset } from "react-icons/gr"
+import PhoneChangeModal from './component/PhoneChangeModal'
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
-  const [recording, setRecording] = useState(false);
-  const [audioBlob, setAudioBlob] = useState(null);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
-  const audioRef = useRef(null);
+  const [view, setView] = useState(false)
+  const [phone, setPhone] = useState('01717642515')
+  const [recording, setRecording] = useState(false)
+  const [audioBlob, setAudioBlob] = useState(null)
+  const [audioDataBuffer, setAudioDataBuffer] = useState(null);
+  const [mediaRecorder, setMediaRecorder] = useState(null)
+  const audioRef = useRef(null)
+
 
   useEffect(() => {
     if (!mediaRecorder) {
       navigator.mediaDevices.getUserMedia({ audio: true })
         .then((stream) => {
           const recorder = new MediaRecorder(stream);
-          const chunks = [];
+          const chunks = []
 
           recorder.ondataavailable = (e) => {
             if (e.data.size > 0) {
               chunks.push(e.data);
             }
-          };
+          }
 
           recorder.onstop = () => {
             const blob = new Blob(chunks, { type: 'audio/wav' });
             setAudioBlob(blob);
-          };
+            const reader = new FileReader();
+            reader.onloadend = () => {
+              setAudioDataBuffer(reader.result);
+            };
+            reader.readAsArrayBuffer(blob);
+          }
 
-          setMediaRecorder(recorder);
+          setMediaRecorder(recorder)
         })
         .catch((err) => console.error('Error accessing microphone:', err));
     }
-  }, [mediaRecorder]);
+  }, [mediaRecorder])
+
 
   const startRecording = () => {
     if (mediaRecorder) {
-      mediaRecorder.start();
-      setRecording(true);
+      mediaRecorder.start()
+      setRecording(true)
     }
-  };
+
+  }
 
   const stopRecording = () => {
     if (mediaRecorder && mediaRecorder.state === 'recording') {
-      mediaRecorder.stop();
-      setRecording(false);
+      mediaRecorder.stop()
+      setRecording(false)
     }
-  };
+  }
 
   const downloadAudio = () => {
-    const url = URL.createObjectURL(audioBlob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'recorded_audio.wav';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  };
+    const url = URL.createObjectURL(audioBlob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'recorded_audio.wav'
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+  }
 
   const playAudio = () => {
-    audioRef.current.src = URL.createObjectURL(audioBlob);
-    audioRef.current.play();
-  };
+    audioRef.current.src = URL.createObjectURL(audioBlob)
+    audioRef.current.play()
+  }
 
   const stopAudio = () => {
     if (audioRef.current) {
-      audioRef.current.pause();
+      audioRef.current.pause()
     }
-  };
+  }
+
+  const analyzeAudio=async()=>{
+    try {
+      //api url end point
+      const res = axios.post(``,{})
+
+      //succeessfully message
+      toast.success('Successfull message')
+    } catch (error) {
+      toast.error('Somthing went wrong.')
+    }
+  }
 
   return (
     <div
-      className='h-screen flex justify-center items-center bg-teal-50'
+      className='h-screen flex items-center'
     >
+
       <div
-        className='w-2/4 pt-4 pb-10 space-y-12 bg-white shadow-md rounded-md'
+        className='w-11/12 mx-auto md:w-2/4 pt-5 pb-40 space-y-12 bg-white rounded-md'
       >
         <div
-          className='flex justify-center items-center space-x-4'
+          className='flex flex-col justify-center items-center space-x-4'
         >
           {recording ?
             <button
@@ -102,6 +129,15 @@ const App = () => {
             </button>
 
           }
+          {recording &&
+            <p
+              className='py-2 text-sm text-gray-400 animate-pulse tracking-widest'
+            >
+              Recording ...
+            </p>
+
+          }
+
         </div>
         <div
           className='space-y-4'
@@ -141,7 +177,7 @@ const App = () => {
             <button
               className='px-4 py-2 bg-teal-500 hover:bg-teal-600 text-white rounded-md shadow-md'
             >
-              Convert Now
+              Analyze audio
             </button>
             <button
               onClick={downloadAudio}
@@ -150,8 +186,22 @@ const App = () => {
               Download
             </button>
           </div>
+          <div
+            className='text-center space-x-2'
+          >
+            <button
+              onClick={()=>setView(!view)}
+              className='px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md shadow-md'
+            >
+              Change Phone Number
+            </button>
+          </div>
         </div>
       </div>
+      {view &&
+        <PhoneChangeModal {...{ view, setView, phone, setPhone }} />
+      }
+      <Toaster/>
     </div>
   );
 };
